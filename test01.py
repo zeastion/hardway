8bcabdbf0d55754376e30f049b45209912d768be
zeastion <zeastion@live.cn>
2013-12-08 14:53:45 +0800

    test0-6

---
+++ test01.py
@@ -0,0 +1,10 @@
+my_name = 'Zeastion'
+my_height = 178
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = "Black"
+
+print "Let's talk about %s." % my_name
+print "He's %dcm tall." % my_height
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee :-)" % my_teeth
---
+++ test02.py
@@ -0,0 +1,5 @@
+Num1 = 10.0 / 3.0
+print "My answer is Num1 = %r" % Num1
+
+Num2 = round(Num1)
+print "Also Num1 =  %r" % Num2
